4cc268d6a07901a6997e81fd9a392538d23f5d48
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-29 10:05:27 +0000

    min cost climbing stairs added

--- DP/OneDimentionalDP.py
+++ DP/OneDimentionalDP.py
@@ -10,5 +10,21 @@ def climbing_stairs(N):
         return cache[n]
     return recursion(N)
 
-print(climbing_stairs(4))
+# print(climbing_stairs(4))
 
+def min_cost_climbing_stairs(cost):
+    n = len(cost)
+    dp = [0] * n
+    if len(cost) == 1:
+        return cost[0]
+    if len(cost) == 2:
+        return min(cost[0], cost[1])
+    dp[0] = cost[0]
+    dp[1] = cost[1]
+    for i in range(2, n):
+        dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+    return min(dp[n-1], dp[n-2])
+
+    
+
+print(min_cost_climbing_stairs([10, 15, 20]))
