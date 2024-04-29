678ee818251e1a4bd37e6cc23b47f01bb5d83550
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-29 09:38:01 +0000

    added implementation for climbing stairs

--- DP/OneDimentionalDP.py
+++ DP/OneDimentionalDP.py
@@ -1,28 +1,14 @@
-class SearchProblem(object):
-    def __init__(self, N):
-        self.N = N
-    
-    def startState(self):
-        return 1
-    def isEndState(self, state):
-        return state == self.N
-    def getSuccessors(self, state):
-        result = []
-        if state + 1 <= self.N:
-            result.append(state + 1)
-        if state + 2 <= self.N:
-            result.append(state + 2)
-        return result
-        
-
-def climbingStairs(problem):
+def climbing_stairs(N):
     cache = {}
-    def futureCost(state):
-        if problem.isEndState(state):
+    def recursion(n):
+        if n == 0 or n == 1:
             return 1
-        if state in cache:
-            return cache[state]
-        result = sum(futureCost(nextState) for nextState in problem.getSuccessors(state))
-        cache[state] = result
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = recursion(n-1)+ recursion(n-2)
+        return cache[n]
+    return recursion(N)
 
+print(climbing_stairs(4))
 
