feac73929e980d8458cb17a6f4eb207fcc9d0496
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-30 05:10:55 +0000

    remove the implementation of min cost stairs

--- DP/OneDimentionalDP.py
+++ DP/OneDimentionalDP.py
@@ -9,9 +9,9 @@ class SearchProblem(object):
         result = []
 
         if state + 1 <= self.N:
-            result.append(('One Step', (state + 1), costList[state+1]))
+            result.append(('One Step', (state + 1), costList[state]))
         if state + 2 <= self.N:
-            result.append(('Two Step', (state + 2), costList[state+2]))
+            result.append(('Two Step', (state + 2), costList[state]))
         return result
 
 def climbing_stairs(N):
@@ -28,22 +28,8 @@ def climbing_stairs(N):
 
 # print(climbing_stairs(4))
 
-def min_cost_climbing_stairs(problem, costList):
-    minCost = float('inf')  # Initialize minCost with a large value
-    def futureCost(state, totalCost, history):
-        nonlocal minCost
-        if problem.isEndState(state):
-            if totalCost < minCost:
-                minCost = totalCost
-            return
-        for action, nextState, cost in problem.getSuccAndCost(state, costList):
-            futureCost(nextState, totalCost + cost, history + [action])
-    futureCost(problem.startState(), 0, [])
-    return minCost
 
-searchList = [10,15,25]
-searchProblem = SearchProblem(len(searchList)-1)
-print(min_cost_climbing_stairs(searchProblem, searchList))
+
         
 
 
