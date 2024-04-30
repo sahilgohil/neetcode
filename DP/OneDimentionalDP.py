a4f5a5167fa9b45e9ee227c3af9432cce1709398
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-04-30 06:25:59 +0000

    adding the sudo code for min cost climbing stairs top down approach

--- DP/OneDimentionalDP.py
+++ DP/OneDimentionalDP.py
@@ -13,6 +13,20 @@ def climbing_stairs(N):
 
 # print(climbing_stairs(4))
 
+"""
+maintain a cache
+get the total length of the costs
+    create a new function recursion that takes the index i
+        if i is in the cache return the cache value
+        if i is less than 0 then return 0
+
+        calculate the cost for current step with cost for current step plus cost of previous step
+        calculate the cost for current step with cost for current step plus cost of previous two step
+        take the minimum for the above two
+        store it in the cache
+        return the cache value
+    call the recursion function for one step and two step and return the minimum of the two
+"""
 def min_cost_climbing_stairs(costs):
     cache = {}
     n = len(costs)
@@ -28,7 +42,15 @@ def min_cost_climbing_stairs(costs):
         return cache[i]
     return min(recursion(n-1), recursion(n-2))
 
-print(min_cost_climbing_stairs([10,15,20]))
+def min_cost_climbing_stairs_bottom_up(costs):
+    dp = [0] * len(costs)
+    dp[0] = costs[0]
+    dp[1] = costs[1]
+    for i in range(2, len(costs)):
+        dp[i] = costs[i] + min(dp[i-1], dp[i-2])
+    return min(dp[len(costs)-1], dp[len(costs)-2])
+
+print(min_cost_climbing_stairs_bottom_up([10,15,20]))
 
 
         
