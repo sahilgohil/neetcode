846bfa0cd79b28bc3e64686baf2aee5b02692bf2
sahilgohil <sahil.gohil11@gmail.com>
2024-05-03 11:57:13 +1000

    practice palindrome count

--- DP/practice.py
+++ DP/practice.py
@@ -109,7 +109,29 @@ def longestPalindrome(s:str)->str:
                         start, end = i, j
 
     return s[start:end+1]
-print(longestPalindrome("babad"))
+# print(longestPalindrome("babad"))
+
 # count the palindromic substrings
+'''
+this question is the same as the previous one we only need to count it and return the value
+
+'''
+def palindromeCount(s:str)->int:
+    n = len(s)
+    dp = [[False] * n for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = True
+    count = n
+    for l in range(2, n+1):
+        for i in range(n-l +1):
+            j = i+l-1
+            if s[i] == s[j]:
+                if l == 2 or dp[i+1][j-1]:
+                    dp[i][j]= True
+                    count = count +1
+
+    return count
+print(palindromeCount("abc"))
 # decode ways
 # coin change
