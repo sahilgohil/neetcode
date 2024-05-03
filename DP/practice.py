ed63b82979315630c5ed0f7f27b4b6c36648eba0
sahilgohil <sahil.gohil11@gmail.com>
2024-05-03 11:41:21 +1000

    practice house robber 1

--- DP/practice.py
+++ DP/practice.py
@@ -31,8 +31,23 @@ def minCostClimbingStairs( cost: List[int]) -> int:
     for i in range(2, n):
         dp[i] = cost[i] + min(dp[i-1], dp[i-2])
     return min(dp[-1],dp[-2])
-print(minCostClimbingStairs([10,15,20]))
+# print(minCostClimbingStairs([10,15,20]))
 # house robber
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+'''
+def houseRobber(money:List[int])->int:
+    n = len(money)
+    dp = [0] * n
+    dp[0] = money[0]
+    dp[1] = max(money[0],money[1])
+
+    for i in range(2, n):
+        dp[i] = max(money[i] + dp[i-2], dp[i-1])
+    return dp[-1]
+print(houseRobber([1,2,3,1]))
 # house robber 2
 # longest palindromic substring
 # count the palindromic substrings
