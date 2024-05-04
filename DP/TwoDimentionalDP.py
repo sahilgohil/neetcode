bfff80f2ad49eea3e70f0565aabf450fa514c33f
sahilgohil <sahil.gohil11@gmail.com>
2024-05-04 12:12:23 +1000

    added sudo code for coin change possible combinations

--- DP/TwoDimentionalDP.py
+++ DP/TwoDimentionalDP.py
@@ -55,6 +55,10 @@ there are three states that we need to keep track of
 
 return the max of sell, cooldown
 
+SUDO Code 
+n is the length of the prices
+dp_buy = 
+
 '''
 
 def maxProfit(prices: List[int]) -> int:
@@ -69,4 +73,37 @@ def maxProfit(prices: List[int]) -> int:
         dpCooldown[i] = max(dpCooldown[i-1], dpSell[i-1]) # cooldown will have max profit after selling or previous cooldown
 
     return max(dpSell[-1],dpCooldown[-1])
-print(maxProfit([1,2,3,0,2]))
+# print(maxProfit([1,2,3,0,2]))
+
+'''
+Problem - Coin Change II
+given the amount and unlimited set of coins, return the possible combinations available that make up to that amount
+
+here we make dp of length amount + 1 with default value of 0, we increase the size of dp because, we need to accomodate the amount 0
+base case, amount 0 will require only 1 combinations that by putting no coins in the combinations
+
+SUDO Code
+n is (amount + 1)
+dp is [0] * n
+base case dp[0] is 1
+
+loop for each coin
+    for amountA from coin to the targetAmount
+        increment the previous possible combinations available for the amount using the coin
+return the dp[amount]
+'''
+
+def change(amount: int, coins: List[int]) -> int:
+    n = amount + 1
+    dp = [0] * n # dp is the number of combination for the amount
+    dp[0] = 1 
+
+    # to find all the combination for the amount we have to loop through each coin
+    for coin in coins:
+        # then we have to check each amount from that coin to our target amount
+        for a in range(coin, n):
+            # add the values of the previous combinations for the coin of this amount
+            dp[a] += dp[a - coin]
+    return dp[amount]
+
+print(change(5, [1,2,5]))
