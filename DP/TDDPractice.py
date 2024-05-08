8ed292c9bc785c942ecfd4b269a02ad56760f1c1
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-08 05:42:15 +0000

    practice max profit

--- DP/TDDPractice.py
+++ DP/TDDPractice.py
@@ -1,3 +1,4 @@
+from typing import List
 '''
 There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
@@ -36,3 +37,25 @@ def longestCommonSubsequence(text1: str, text2: str) -> int:
                 dp[i][j] = max(dp[i-1][j],dp[i][j-1])
     return dp[-1][-1]
 
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+'''
+
+def maxProfit(prices: List[int]) -> int:
+    n = len(prices)
+    dpBuy = [0] * n
+    dpSell = [0] * n
+    dpCooldown = [0] * n
+    # all the above three represents the max rpofit at the ith index
+    # basecase 
+    dpBuy[0] = -prices[0]
+    for i in range(1,n):
+        dpBuy[i] = max(dpBuy[i-1], dpCooldown[i-1] - prices[i])
+        dpSell[i] = dpBuy[i-1] + prices[i]
+        dpCooldown[i] = max(dpCooldown[i-1], dpSell[i-1])
+    return max(dpSell[-1], dpCooldown[-1])
--- DP/TwoDimentionalDPTest.py
+++ DP/TwoDimentionalDPTest.py
@@ -1,9 +1,11 @@
 import unittest
-from TDDPractice import uniquePaths,longestCommonSubsequence
+from TDDPractice import uniquePaths,longestCommonSubsequence,maxProfit
 
 class TestTwoDimentionalDP(unittest.TestCase):
     def test_uniqpaths(self):
         self.assertEqual(uniquePaths(3,7),28)
     def test_longestCommonSubsequence(self):
         self.assertEqual(longestCommonSubsequence("abcde", "ace"),3)
+    def test_maxprofit(self):
+        self.assertEqual(maxProfit([1,2,3,0,2]),3)
 unittest.main()
