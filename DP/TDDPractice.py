71b2417e91732dfd5273e071a68f09f2d3180036
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-08 05:34:22 +0000

    practice longest common subsequence

--- DP/TDDPractice.py
+++ DP/TDDPractice.py
@@ -13,3 +13,26 @@ def uniquePaths(m: int, n: int) -> int:
     
     return dp[-1][-1]
 
+'''
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+'''
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    # increased the length to accomodate for the base case of empty strings
+    m = len(text1) + 1
+    n = len(text2) + 1
+
+    dp = [[0]*n for _ in range(m)]
+
+    for i in range(1,m):
+        for j in range(1, n):
+            if text1[i-1] == text2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    return dp[-1][-1]
+
--- DP/TwoDimentionalDPTest.py
+++ DP/TwoDimentionalDPTest.py
@@ -1,8 +1,9 @@
 import unittest
-from TDDPractice import uniquePaths
+from TDDPractice import uniquePaths,longestCommonSubsequence
 
 class TestTwoDimentionalDP(unittest.TestCase):
     def test_uniqpaths(self):
         self.assertEqual(uniquePaths(3,7),28)
-
+    def test_longestCommonSubsequence(self):
+        self.assertEqual(longestCommonSubsequence("abcde", "ace"),3)
 unittest.main()
