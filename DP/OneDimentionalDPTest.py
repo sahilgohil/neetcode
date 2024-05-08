4e236b03350534ad910015c1e1203a3a2061815c
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-08 00:55:45 +0000

    practic house robber

--- DP/OneDimentionalDPTest.py
+++ DP/OneDimentionalDPTest.py
@@ -1,5 +1,5 @@
 from OneDimentionalDP import climbing_stairs
-from practice import minCostClimbingStairs
+from practice import minCostClimbingStairs, houseRobber
 import unittest
 
 class TestClimbingStairs(unittest.TestCase):
@@ -9,5 +9,7 @@ class TestClimbingStairs(unittest.TestCase):
     def test_min_cost_climbing_stairs(self):
         self.assertEqual(minCostClimbingStairs([10, 15, 20]), 15)
         self.assertEqual(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
-
+    def test_house_robber(self):
+        self.assertEqual(houseRobber([1, 2, 3, 4, 5]), 9)
+        self.assertEqual(houseRobber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 30)
 unittest.main()
--- DP/practice.py
+++ DP/practice.py
@@ -42,14 +42,17 @@ You are a professional robber planning to rob houses along a street. Each house 
 Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 '''
 def houseRobber(money:List[int])->int:
-    n = len(money)
+    n =  len(money)
     dp = [0] * n
+    # base cases
     dp[0] = money[0]
-    dp[1] = max(money[0],money[1])
+    dp[1] = max(money[0], money[1])
 
     for i in range(2, n):
         dp[i] = max(money[i] + dp[i-2], dp[i-1])
+
     return dp[-1]
+
 # print(houseRobber([1,2,3,1]))
 
 # house robber 2
