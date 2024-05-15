6e3b5cfc532a2adc9e1d15f82a52a4d3d5e04b8c
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-15 06:53:04 +0000

    added implementation of maxCoins

--- DP/TwoDimentionalDP.py
+++ DP/TwoDimentionalDP.py
@@ -239,5 +239,19 @@ def minDistance(word1: str, word2: str) -> int:
 
 
 '''
-
+Burst Balloons
 '''
+def maxCoins(nums: List[int]) -> int:
+    cache = {} #(l,r) -> total where l is the left after added 1 and r is the last before added 1
+    nums = [1] + nums + [1]
+    def dfs(l,r):
+        if l>r:
+            return 0
+        if (l,r) in cache:
+            return cache[(l,r)]
+        cache[(i,j)] = 0
+        for i in range(l,r+1):
+            cache[(i,j)] = nums[l-1] * nums[i] * nums[r+1]
+            cache[(i,j)] = dfs(l,i-1) + dfs(i+1,r)
+        return cache[(i,j)]
+    return dfs(1,len(nums)-2)
