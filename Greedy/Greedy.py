eb9bb12ccc235f61f111502e6490ebc2e3d36569
sahilgohil <sahil.gohil11@gmail.com>
2024-05-16 20:35:02 +1000

    implemented gas station

--- Greedy/Greedy.py
+++ Greedy/Greedy.py
@@ -36,5 +36,54 @@ def canJump(nums: List[int]) -> bool:
         if i + nums[i] >= goal:
             goal = i
     return True if goal == 0 else False
-print(canJump([3,2,1,1,4]))
-print(canJump([3,2,1,0,4]))
+# print(canJump([3,2,1,1,4]))
+# print(canJump([3,2,1,0,4]))
+
+'''
+Jump Game 2
+
+find the min jumps to reach the last index
+
+'''
+
+def jump(nums: List[int]) -> int:
+    res = 0
+
+    l = r = 0
+    while r < len(nums) -1:
+        farthest = 0
+        for i in range(l ,r+1):
+            farthest = max(farthest, i+nums[i])
+        l = r+1
+        r = farthest
+        res += 1
+    return res
+
+# print(jump([2,3,1,1,4]))
+
+'''
+
+Gas Station
+
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+'''
+
+def canCompleteCircuit(gas: List[int], cost: List[int]) -> int:
+    if sum(gas) < sum(cost): # if i dont have enough gas then return 0
+        return -1
+    total = 0
+    start = 0
+    for i in range(len(gas)):
+        total += (gas[i] - cost[i])
+
+        if total <0:
+            total = 0
+            start = i+1
+
+    return start
+print(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))
