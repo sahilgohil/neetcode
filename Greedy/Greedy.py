945c0a805fa6a9ab5d80f0236f5a00871e9c7a25
sahilgohil <sahil.gohil11@gmail.com>
2024-05-16 20:02:31 +1000

    implemented the can jump

--- Greedy/Greedy.py
+++ Greedy/Greedy.py
@@ -15,3 +15,26 @@ def maxSubArray(nums: List[int]) -> int:
         currSum += nums[i]
         currMax = max(currMax,currSum)
     return currMax
+
+'''
+Jump Game
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+
+Solution is :
+start from the last index and check if the jump from that index is greater or equal to the goal then we can shift the goal to closer point to starting position 
+
+at the end of the loop if we reach the end 0 then we return the true and false otherwise
+'''
+def canJump(nums: List[int]) -> bool:
+    goal = len(nums) -1
+
+    for i in range(len(nums) -1,-1,-1):
+        if i + nums[i] >= goal:
+            goal = i
+    return True if goal == 0 else False
+print(canJump([3,2,1,1,4]))
+print(canJump([3,2,1,0,4]))
--- Greedy/GreedyTest.py
+++ Greedy/GreedyTest.py
@@ -1,9 +1,12 @@
 import unittest
 
-from Greedy import maxSubArray
+from Greedy import maxSubArray, canJump
 
 class TestGreedy(unittest.TestCase):
 
     def test_max_sum_subarray(self):
         self.assertEqual(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]),6)
+    def test_can_jump(self):
+        self.assertEqual(canJump([3,2,1,1,4]),True)
+        self.assertEqual(canJump([3,2,1,0,4]),False)
 unittest.main()
