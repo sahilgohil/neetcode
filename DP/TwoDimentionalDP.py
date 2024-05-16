2ede101395296f266c96f36e8d26797ec0954fbf
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-16 03:47:50 +0000

    completed implementation of pattern matching

--- DP/TwoDimentionalDP.py
+++ DP/TwoDimentionalDP.py
@@ -266,7 +266,7 @@ def isMatch(s: str, p: str) -> bool:
     def dfs(i,j):
         if i >= len(s) and j >= len(p):
             return True
-        if j >= len(s):
+        if j >= len(s) or j >= len(p):
             return False
         if (i,j) in cache:
             return cache[(i,j)]
@@ -274,7 +274,10 @@ def isMatch(s: str, p: str) -> bool:
 
         # if the next char is *
         if j+1 < len(p) and p[j+1] == "*":
-            
-        
+            cache[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
+            return cache[(i,j)]
+        if match:
+            cache[(i,j)] = dfs(i+1,j+1)
+            return cache[(i,j)]
         return False
     return dfs(0,0)
