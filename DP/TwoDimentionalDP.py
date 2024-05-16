670ab12e742e50518435188093851177f2e2159a
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-16 06:36:35 +0000

    added implementation for reg expression matching

--- DP/TwoDimentionalDP.py
+++ DP/TwoDimentionalDP.py
@@ -262,22 +262,24 @@ def maxCoins(nums: List[int]) -> int:
 Pattern matching expression
 '''
 def isMatch(s: str, p: str) -> bool:
-    cache = {} # (i,j) -> bool
-    def dfs(i,j):
-        if i >= len(s) and j >= len(p):
-            return True
-        if j >= len(s) or j >= len(p):
-            return False
-        if (i,j) in cache:
-            return cache[(i,j)]
-        match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+        cache = {} # (i,j) -> bool
+        def dfs(i,j):
+            if (i,j) in cache:
+                return cache[(i,j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+        
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
 
-        # if the next char is *
-        if j+1 < len(p) and p[j+1] == "*":
-            cache[(i,j)] = dfs(i,j+2) or (match and dfs(i+1,j))
+            # if the next char is *
+            if (j+1) < len(p) and p[j+1] == "*":
+                cache[(i,j)] = dfs(i, j+2) or (match and dfs(i + 1, j))   # use *
+                return cache[(i,j)]
+            if match:
+                cache[(i,j)] = dfs(i+1, j+1)
+                return cache[(i,j)]
+            cache[(i,j)] = False
             return cache[(i,j)]
-        if match:
-            cache[(i,j)] = dfs(i+1,j+1)
-            return cache[(i,j)]
-        return False
-    return dfs(0,0)
+        return dfs(0,0)
