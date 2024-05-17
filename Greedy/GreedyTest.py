9cdbdc46f6f9e9319653a49d68c025da63a24db9
sahilgohil <sahil.gohil11@gmail.com>
2024-05-17 19:47:43 +1000

    implemented merge triplets

--- Greedy/GreedyTest.py
+++ Greedy/GreedyTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Greedy import maxSubArray, canJump,isNStraightHand
+from Greedy import maxSubArray, canJump,isNStraightHand,mergeTriplets
 
 class TestGreedy(unittest.TestCase):
 
@@ -12,4 +12,8 @@ class TestGreedy(unittest.TestCase):
     def test_hand_of_straight(self):
         self.assertEqual(isNStraightHand([1,2,3,6,2,3,4,7,8],3),True)
         self.assertEqual(isNStraightHand([1,2,3,4,5],4),False)
+    def test_merge_triplets(self):
+        self.assertEqual(mergeTriplets([[2,5,3],[1,8,4],[1,7,5]],[2,7,5]), True)
+        self.assertEqual(mergeTriplets([[3,4,5],[4,5,6]], [3,2,5]), False)
+
 unittest.main()
