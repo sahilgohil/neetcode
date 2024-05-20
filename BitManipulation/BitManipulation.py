d0190103402cf60df5e860161b2a95fd83eec292
sahilgohil <sahil.gohil11@gmail.com>
2024-05-20 20:19:51 +1000

    rest of the commit

--- BitManipulation/BitManipulation.py
+++ BitManipulation/BitManipulation.py
@@ -27,3 +27,40 @@ def hammingWeight(n: int) -> int:
         res += n % 2
         n = n >> 1
     return res
+
+'''
+Counting Bits
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+'''
+def countBits(n: int) -> List[int]:
+    dp = [0] * (n+1)
+    offset = 1
+    for i in range(1, len(dp)):
+        if offset * 2 == i:
+            offset = i
+        dp[i] = dp[i-offset]
+        
+    return dp
+
+
+'''
+Reverse Bits
+
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ 
+'''
+
+def reverseBits(n: int) -> int:
+    res = 0
+
+    for i in range(32):
+        bit = (n >> i) & 1
+        res = res | (bit << (31-i))
+
+    return res
