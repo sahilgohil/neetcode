77d2458df44e76df31007c204846ffc1558d07bb
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-20 03:12:28 +0000

    implemented clone graph

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -43,3 +43,56 @@ def numIslands(grid: List[List[str]]) -> int:
                 islands += 1
     
     return islands
+
+'''
+Max Area Of Island
+'''
+def maxAreaOfIsland(grid: List[List[int]]) -> int:
+    maxArea = 0
+    visited = set()
+
+    rows, cols = len(grid), len(grid[0])
+ 
+    def dfs(i,j):
+        if i not in range(rows) or j not in range(cols) or grid[i][j] == 0 or (i,j) in visited:
+            return 0
+        visited.add((i,j))
+        
+        return (1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j + 1) + dfs(i,j - 1))
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1 and (r,c) not in visited:
+            
+                ca = dfs(r,c)
+                maxArea = max(maxArea, ca)
+    return maxArea
+
+
+'''
+Clone Graph
+
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+'''
+
+def cloneGraph(node: Optional['Node']) -> Optional['Node']:
+    oldToNew = {} #oldNode -> newNode
+
+    def dfs(node):
+        if node in oldToNew:
+            return oldToNew[node]
+        copy = Node(node.val)
+        oldToNew[node] = copy
+        for nei in node.neighbors:
+            copy.neighbors.append(dfs(nei))
+        return copy
+    return dfs(node)
--- Graph/GraphTest.py
+++ Graph/GraphTest.py
@@ -1,8 +1,11 @@
 import unittest
-from Graph import numIslands
+from Graph import numIslands, maxAreaOfIsland
 class TestGraph(unittest.TestCase):
     def test_num_islands(self):
         self.assertEqual(numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]),1)
         self.assertEqual(numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]),3)
+    def test_max_area_of_island(self):
+        self.assertEqual(maxAreaOfIsland([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]),6)
+        
 if __name__ == '__main__':
     unittest.main()
