c98c792dbd416732bae2d6712b0eb34590e0a577
sahilgohil <sahil.gohil11@gmail.com>
2024-05-21 16:06:05 +1000

    Implement course scheduling algorithm

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -263,3 +263,43 @@ def solve(board: List[List[str]]) -> None:
         for c in range(cols):
             if board[r][c] == "T":
                 board[r][c] = "O"
+
+
+'''
+Course Schedule
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+'''
+
+def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+    # create a map for each course as key and its prerequisits as values
+    preMap = {i:[] for i in range(numCourses)}
+    # loop through the prerequisits and put values
+    for cors,pre in prerequisites:
+        preMap[cors].append(pre)
+    
+    # create a dfs function that loops and checks for any loops
+    visit = set() # set of all the visited courses
+
+    def dfs(cors):
+        if cors in visit:
+            return False
+        if preMap[cors] == []:
+            return True
+        
+        visit.add(cors)
+        for pre in preMap[cors]:
+            if not dfs(pre): # if any of the pre requsits are visited already then return false
+                return False
+        visit.remove(cors)
+        preMap[cors] = []
+        return True
+    
+    for cors in range(numCourses):
+        if not dfs(cors):
+            return False
+        
+    return True
