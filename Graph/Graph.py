3507432ede469866f9ca5022f8d25e0f593e92d7
sahilgohil <sahil.gohil11@gmail.com>
2024-05-21 14:19:33 +1000

    implemented pacific atlantic water flow

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -107,7 +107,7 @@ You are given a m x n 2D grid initialized with these three possible values.
 INF - Infinity means an empty room. We use the value 2^31 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
 Fill each empty room with the distance to its nearest gate. If it is impossible to reach a Gate, that room should remain filled with INF
 '''
-def walls_and_gates(grid: List[List[int]]) -> List[List[int]]:
+def walls_and_gates(grid: List[List[int]]) -> List[List[int]]: # type: ignore
     
     rows,cols = len(grid), len(grid[0])
     q = collections.deque()
@@ -182,4 +182,47 @@ def orangesRotting(grid: List[List[int]]) -> int:
         time+=1
 
     return time if freahOranges == 0 else -1
-print(orangesRotting([[1,2]]))
+# print(orangesRotting([[1,2]]))
+
+'''
+
+Pacific Atlantic waterflow
+
+There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+
+The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+'''
+def pacificAtlantic(heights: List[List[int]]) -> List[List[int]]:
+    res = []
+
+    rows,cols = len(heights), len(heights[0])
+
+    pac, atl = set(), set()
+
+    def dfs(r,c,visit,preHeight):
+        if r not in range(rows) or c not in range(cols) or (r,c) in visit or heights[r][c] < preHeight:
+            return
+        visit.add((r,c))
+        dfs(r+1,c,visit,heights[r][c])
+        dfs(r-1,c,visit,heights[r][c])
+        dfs(r,c+1,visit,heights[r][c])
+        dfs(r,c-1,visit,heights[r][c])
+
+    for c in range(cols):
+        dfs(0,c,pac,heights[0][c])
+        dfs(rows-1,c,atl,heights[rows-1][c])
+
+    for r in range(rows):
+        dfs(r, 0, pac, heights[r][0])
+        dfs(r, cols-1,atl,heights[r][cols-1])
+
+    for r in range(rows):
+        for c in range(cols):
+            if (r,c) in pac and (r,c) in atl:
+                res.append([r,c])
+
+    return res
