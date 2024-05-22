93d474b5f7143b022ea7aa8eb41d0c34a61bd2dd
sahilgohil <sahil.gohil11@gmail.com>
2024-05-22 17:05:48 +1000

    feat: Implement countComponents function in Graph.py

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -379,5 +379,22 @@ There is an undirected graph with n nodes. There is also an edges array, where e
 Return the total number of connected components in that graph.
 '''
 def countComponents(n: int, edges: List[List[int]]) -> int:
+    visit = set()
+    adjList = {i:[] for i in range(n)}
+    for n1,n2 in edges:
+        adjList[n1].append(n2)
+        adjList[n2].append(n1)
     
-    return 0
+    def dfs(node):
+        if node in visit:
+            return
+        visit.add(node)
+        for nei in adjList[node]:
+           if nei not in visit:
+               dfs(nei)
+    components = 0
+    for i in range(n):
+        if i not in visit:
+            components += 1
+            dfs(i)
+    return components
