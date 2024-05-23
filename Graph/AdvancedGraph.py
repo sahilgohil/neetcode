756c1e2869dcd9fdfb620a5314cc952d88276437
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-23 01:51:42 +0000

    implemented min cost to connect all components

--- Graph/AdvancedGraph.py
+++ Graph/AdvancedGraph.py
@@ -36,3 +36,49 @@ def findItinerary(tickets: List[List[str]]) -> List[str]:
 
     # return the reverse of the result
     return res[::-1]
+
+'''
+
+Min Cost to connect all points
+
+You are given an array points representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi].
+
+The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
+
+Return the minimum cost to make all points connected. All points are connected if there is exactly one simple path between any two points.
+'''
+
+# solution requires prim's algorithm for min heap
+
+def minCostConnectPoints(points: List[List[int]]) -> int:
+    # create a map
+    adjList = {i:[] for i in range(points)}
+
+    # assign the manhatten distance to each node and make neighbours
+    N = len(points)
+    
+    for i in range(N):
+        x1, y1 = points[i]
+        for j in range(i+1, N):
+            x2, y2 = points[j]
+            cost = abs(x1-x2) + abs(y1-y2)
+            # it is undirected graph
+            adjList[i].append([cost,j])
+            adjList[j].append([cost,i])
+
+    # Prim's Algo
+    res = 0
+    minH = [[0,0]] # starting at node 0 with distance cost 0 [cost, node]
+    visit = set()
+    
+    while len(visit) < N:
+        cost, i = heapq.heappop(minH)
+        if i in visit:
+            continue
+        visit.add(i)
+        res += cost
+        for neiCost, nei in adjList[i]:
+            if nei not in visit:
+                heapq.heappush(minH, [neiCost,nei])
+
+    return res
