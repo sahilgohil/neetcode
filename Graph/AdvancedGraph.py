9314c4f2c82dd8a50f02ddfc53d9a9c56dcad5e3
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-23 02:31:24 +0000

    Implement dikstra algo

--- Graph/AdvancedGraph.py
+++ Graph/AdvancedGraph.py
@@ -82,3 +82,30 @@ def minCostConnectPoints(points: List[List[int]]) -> int:
                 heapq.heappush(minH, [neiCost,nei])
 
     return res
+
+
+'''
+Network Delay Time
+
+You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+
+We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+'''
+
+def networkDelayTime(times: List[List[int]], n: int, k: int) -> int:
+    adjList = collections.defaultdict(list)
+    for u,v,w in times:
+        adjList[u].append((v,w)) #(dest, weight)
+    t = 0
+    minH = [[0,0]] # cost,node
+    visit = set()
+
+    while minH:
+        cost, node = heapq.heappop(minH)
+        if node in visit:
+            continue
+        visit.add(node)
+        t+=cost
+        for nei, weight in adjList[node]:
+            heapq.heappush([cost+weight,nei])
+    return t
