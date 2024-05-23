4c16ac7109a9bf118c3e5c0158ef5efab28ae88e
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-23 01:11:49 +0000

    implement the finditenary

---
+++ Graph/AdvancedGraph.py
@@ -0,0 +1,38 @@
+from typing import List
+import collections
+import heapq
+'''
+Reconstruct Itinerary
+
+You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+'''
+def findItinerary(tickets: List[List[str]]) -> List[str]:
+    adjList = collections.defaultdict(list)
+    # create a dictionary of empty lists
+
+    for src, dest in tickets:
+        heapq.heappush(adjList[src],dest) # make it a min heap by pushing values in the top
+    
+    
+    stack = ["JFK"] # create a stack for iteration and push the first value JFK
+    res = [] # keep track ofn the result
+
+    # run while the stack is non empty
+    while stack:
+        # take the last value of the stack
+        src = stack[-1]
+        # check if it is in the dict and it has values in the list
+        if src in adjList and adjList[src]:
+            # append the value from the src's 
+            stack.append(heapq.heappop(adjList[src]))
+        else:
+            # if there are no destinations left then push the result and take the src out of stack
+            res.append(stack.pop())
+
+    # return the reverse of the result
+    return res[::-1]
