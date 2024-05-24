475e6c97f7d68503081d24be64a02c3fca790d08
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-24 05:20:55 +0000

    partition palindrome

--- Backtracking/Backtracking.py
+++ Backtracking/Backtracking.py
@@ -186,4 +186,35 @@ def exist(board: List[List[str]], word: str) -> bool:
                     return True
     return False
 
-print(exist([["C","A","A"],["A","A","A"],["B","C","D"]],"AAB"))
+# print(exist([["C","A","A"],["A","A","A"],["B","C","D"]],"AAB"))
+'''
+Palindrome partition
+
+Given a string s, partition s such that every 
+substring
+ of the partition is a 
+palindrome
+. Return all possible palindrome partitioning of s.
+'''
+def partition( s: str) -> List[List[str]]:
+    res = []
+    part = []
+
+    def backtrack(i):
+        if i >= len(s):
+            res.append(part.copy())
+            return
+        for j in range(i,len(s)):
+            if isPalindrome(s,i,j):
+                part.append(s[i:j+1])
+                backtrack(j+1)
+                part.pop()
+    backtrack(0)
+    return res
+def isPalindrome(s,l,r):
+    while(l<r):
+        if s[l] != s[r]:
+            return False
+        l,r = l+1,r-1
+    return True
+print(partition("aab"))
