0d16aa658a49244e8baa6be4b3b54a1a98b78631
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-24 05:03:35 +0000

    implement the wordsearch problem

--- Backtracking/Backtracking.py
+++ Backtracking/Backtracking.py
@@ -153,4 +153,37 @@ def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
     backtrack(0,0,[])
     return res
 
-print(combinationSum2([10,1,2,7,6,1,5],8))
+# print(combinationSum2([10,1,2,7,6,1,5],8))
+
+'''
+Word Search
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+'''
+
+def exist(board: List[List[str]], word: str) -> bool:
+    rows,cols = len(board), len(board[0])
+    visit = set()
+    def backtrack(r,c,i):
+        if i == len(word):
+            return True
+        if r not in range(rows) or c not in range(cols) or board[r][c] != word[i] or (r,c) in visit:
+            return False
+
+        visit.add((r,c))
+        res = (backtrack(r+1,c,i+1) or
+               backtrack(r-1,c,i+1) or
+               backtrack(r,c+1,i+1) or 
+               backtrack(r,c-1,i+1))
+        visit.remove((r,c))
+        return res
+    for i in range(rows):
+        for j in range(cols):
+            if board[i][j] == word[0]:
+                if backtrack(i,j,0):
+                    return True
+    return False
+
+print(exist([["C","A","A"],["A","A","A"],["B","C","D"]],"AAB"))
