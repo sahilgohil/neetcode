6e75e919246d987d0ed4d8d00185edccc9c94c30
sahil.gohil11 <sahil.gohil11@gmail.com>
2024-05-24 06:35:44 +0000

    implement n queens

--- Backtracking/Backtracking.py
+++ Backtracking/Backtracking.py
@@ -254,5 +254,42 @@ def letterCombinations( digits: str) -> List[str]:
     backtrack(0)
     return res
 
-print(letterCombinations("23"))
+# print(letterCombinations("23"))
+'''
+N-Queens
 
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+'''
+
+def solveNQueens(n: int) -> List[List[str]]:
+
+    col = set()
+    posDia = set()
+    negDia = set()
+    res = []
+    board = [["."]*n for i in range(n)]
+    def backtrack(r):
+        if r == n:
+            copy = ["".join(row) for row in board]
+            res.append(copy)
+            return
+        for c in range(n):
+            if c in col or (r+c) in posDia or (r-c) in negDia:
+                continue
+            
+            col.add(c)
+            posDia.add(r+c)
+            negDia.add(r-c)
+            board[r][c] = "Q"
+            backtrack(r+1)
+            col.remove(c)
+            posDia.remove(r+c)
+            negDia.remove(r-c)
+            board[r][c] = "."
+    backtrack(0)
+    return res
+print(solveNQueens(4))
