205e10881b417bbde9ac7fe9a1ccaa41c62c80ae
sahilgohil <sahil.gohil11@gmail.com>
2024-05-27 11:27:11 +1000

    implemented task scheduler

--- Heap/heap.py
+++ Heap/heap.py
@@ -92,3 +92,31 @@ def findKthLargest(nums: List[int], k: int) -> int:
     while(len(minH) > k):
         heapq.heappop(minH)
     return minH[0]
+
+
+'''
+Task Scheduler
+
+
+'''
+def leastInterval(self, tasks: List[str], n: int) -> int:
+    '''
+    first make the hashmap with key as (taskchar, its priority)
+    every loop we check if the char exist in the hashmap then we take its value
+    and and add n to it and push the new char in the heap
+
+    '''
+    count = collections.Counter(tasks)
+    maxHeap = [-cnt for cnt in count.values()]
+    heapq.heapify(maxHeap)
+    q = collections.deque()
+    time = 0
+    while maxHeap or q:
+        time += 1
+        if maxHeap:
+            cnt = 1 + heapq.heappop(maxHeap)
+            if cnt:
+                q.append([cnt,time+n])
+        if q and q[0][1] == time:
+            heapq.heappush(maxHeap, q.popleft()[0])
+    return time
