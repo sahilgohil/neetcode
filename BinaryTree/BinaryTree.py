fd29e6ad7074fb33111aedf3bb67930f2ed95422
sahilgohil <sahil.gohil11@gmail.com>
2024-05-27 15:14:08 +1000

    implement the maxdepth of binary tree

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -22,3 +22,22 @@ def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         helper(node.right)
     helper(root)
     return root
+
+'''
+Maximum depth of binary tree
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+'''
+
+def maxDepth(root: Optional[TreeNode]) -> int:
+    def dfs(node):
+        if not node:
+            return 0
+        if not node.left and not node.right:
+            return 1
+        return 1 + max(dfs(node.left),dfs(node.right))
+    return dfs(root)
