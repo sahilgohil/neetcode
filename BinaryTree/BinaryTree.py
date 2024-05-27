60af21aa5c2e6037a5b581a2b083997759f6465f
sahilgohil <sahil.gohil11@gmail.com>
2024-05-27 16:05:51 +1000

    feat: Add isBalanced function to check if a binary tree is height-balanced

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -57,3 +57,24 @@ def diameterOfBinaryTree( root: Optional[TreeNode]) -> int:
         return 1 + max(left,right) # return the max height at the particular node
     dfs(root)
     return res[0]
+
+'''
+110. Balanced Binary Tree
+Given a binary tree, determine if it is 
+height-balanced
+.
+'''
+def isBalanced(root: Optional[TreeNode]) -> bool:
+
+    def dfs(root):
+        # if the root is empty then return the tree is balanced and height is -1
+        if not root:
+            return [True, -1]
+        # go to the end of left and right tree
+        left = dfs(root.left)
+        right = dfs(root.right)
+        # check if the subtree is balanced
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        # return the answer with the maxheight at the node
+        return [balanced, 1 + max(left[1],right[1])]
+    return dfs(root)[0]# return the answer
