7982e1b9ec1a66b2f7afa426a989694b1818638b
sahilgohil <sahil.gohil11@gmail.com>
2024-05-27 16:43:31 +1000

    implement the lowest common ancestor

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -108,3 +108,23 @@ def isSubtree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
                 return True
         return dfs(root.left,subRoot) or dfs(root.right,subRoot)
     return dfs(root,subRoot)
+
+
+'''
+Lowest Common Ancestor
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+'''
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    curr = root
+    while curr:
+        if p.val>curr.val and q.val>curr.val:
+            curr = curr.right
+        elif p.val<curr.val and q.val<curr.val:
+            curr = curr.left
+        else:
+            return curr
+    return root
