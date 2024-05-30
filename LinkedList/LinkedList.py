75877d77a75603c3173193deeec1abd1fe839cf2
sahilgohil <sahil.gohil11@gmail.com>
2024-05-30 10:05:28 +1000

    merge two sorted lists

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -21,4 +21,33 @@ def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
 
 
     return newhead
-    
+
+'''
+Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+'''
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(-1)
+    curr = dummy
+    while list1 and list2:
+        if list1.val < list2.val:
+            curr.next = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            list2 = list2.next
+        curr = curr.next
+    while list1:
+        curr.next = list1
+        curr = curr.next
+        list1 = list1.next
+    while list2:
+        curr.next = list2
+        curr = curr.next
+        list2 = list2.next
+    return dummy.next
