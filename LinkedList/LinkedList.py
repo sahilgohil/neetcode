96350a7c2fd548498339ed107c3c8ac9deeb7ac5
sahilgohil <sahil.gohil11@gmail.com>
2024-05-30 15:44:59 +1000

    LRU

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -235,3 +235,66 @@ def findDuplicate(nums: List[int]) -> int:
         s2 = nums[s2]
         if s == s2:
             return s
+'''
+LRU Cache
+
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+'''    
+
+class DoubleNode:
+        def __init__(self, key, value):
+           self.k = key
+           self.v = value
+           self.next = None
+           self.prev = None
+class LRUCache:
+            
+    def __init__(self, capacity: int):
+       self.cap = capacity
+       self.cache = {}
+       self.left = DoubleNode(-1,-1)
+       self.right = DoubleNode(-1,-1)
+       self.left.next = self.right
+       self.right.prev = self.left
+
+    def insert(self,node):
+        rightPrev = self.right.prev
+        node.next = self.right
+        node.prev = rightPrev
+        rightPrev.next = node
+        self.right.prev = node
+        
+    def remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].v
+        return -1
+        
+    def put(self, key: int, value: int) -> None:
+        # remove from the list
+        if key in self.cache:
+            self.remove(self.cache[key])
+        # create new node
+        self.cache[key] = DoubleNode(key,value)
+        self.insert(self.cache[key])
+        
+        # now check if the capacity is reached or not
+        if len(self.cache) > self.cap:
+            removalNode = self.left.next
+            self.remove(removalNode)
+            del self.cache[removalNode.k]
+            
+            
