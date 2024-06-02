9b9bb80686bed0828b45df310e5f3d28a6d38905
sahilgohil <sahil.gohil11@gmail.com>
2024-06-02 11:56:34 +1000

    implement koko eating bananas

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -43,3 +43,30 @@ def searchMatrix(matrix: List[List[int]], target: int) -> bool:
         else:
             r += 1
     return False
+
+'''
+Koko eating bananas
+
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+'''
+def minEatingSpeed(piles: List[int], h: int) -> int:
+    def canEat(k):
+        totalHours = 0
+        for pile in piles:
+            totalHours += (pile + k - 1)//k
+        return totalHours <= h
+    left,right = 1, max(piles)
+    while left < right:
+        mid = (left+right)//2
+        if canEat(mid):
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
