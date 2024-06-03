317f0745991c89a55d9aebbbfcb7c552e9d4c580
sahilgohil <sahil.gohil11@gmail.com>
2024-06-03 18:39:55 +1000

    implement the search in roteted sorted array

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -70,3 +70,60 @@ def minEatingSpeed(piles: List[int], h: int) -> int:
             left = mid + 1
 
     return left
+
+'''
+Find minimum in rotated sorted array
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+'''
+
+def findMin(nums: List[int]) -> int:
+    l,r= 0,len(nums)-1
+    while l<=r:
+        mid= (l+r)//2
+        if nums[mid] > nums[r]:
+            l = mid+1
+        else:
+            r = mid-1
+    return nums[l]
+
+'''
+Search in roteted sorted array
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
+def search(nums: List[int], target: int) -> int:
+   l, r = 0, len(nums)-1
+
+   while l<=r:
+       mid = (l+r)//2
+       if nums[mid] == target:
+           return mid
+       # left is sorted
+       if nums[mid] >= nums[l]:
+           if target < nums[mid] and target >= nums[l]:
+               r = mid-1
+           else:
+               l = mid+1
+       else:
+           if target <= nums[r] and target > nums[mid]:
+               l = mid + 1
+           else:
+               r = mid - 1
+
+   return -1
+           
