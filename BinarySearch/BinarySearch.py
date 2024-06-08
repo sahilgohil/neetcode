a4bfdd9f21235a62601b71402ae0eb6d3a87094c
sahilgohil <sahil.gohil11@gmail.com>
2024-06-09 09:07:07 +1000

    implement median of two sorted array

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -154,3 +154,38 @@ class TimeMap:
            else:
                r = mid-1
        return res
+    
+'''
+Median of two sorted array
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+'''
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    A,B = nums1, nums2
+    if len(B) < len(A):
+        A,B = B,A
+    total = (len(A) + len(B))
+    half = total//2
+
+    l, r = 0, len(A) - 1
+    while True:
+        i = (l+r)//2 # A pointer
+        j = (half-i-2) # B pointer
+
+        Aleft = A[i] if i >=0 else float('-infinity')
+        Aright = A[i+1] if (i + 1) < len(A) else float('infinity')
+        Bleft = B[j] if j >=0 else float('-infinity')
+        Bright = B[j+1] if (j + 1) < len(B) else float('infinity')
+
+        if Aleft <= Bright and Bleft <= Aright:
+            # odd
+            if total % 2:
+                return min(Aright,Bright)
+            else:
+                return (max(Aleft,Bleft) + min(Aright,Bright))//2
+        elif Aleft > Bright:
+            r = i -1
+        else:
+            l = i+1
